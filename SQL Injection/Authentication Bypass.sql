3e3d514f933822921a9741db22378a2b07e983ac
Rasuli Omari <122861790+rasuliomari@users.noreply.github.com>
2025-01-03 19:11:19 +0000

    try to bypass in login form

--- SQL Injection/Authentication Bypass.sql
+++ SQL Injection/Authentication Bypass.sql
@@ -2,3 +2,6 @@
 select * from users where username='%username%' and password='%password%' LIMIT 1;
 # put this on the password
 ' OR 1=1;--
+# return
+
+select * from users where username='' and password='' OR 1=1;
